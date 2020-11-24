d850635ff7ad80dcaeba6cffc0aebb89a883f0fa
AdamChrimes <iChrimes@live.co.uk>
2020-11-24 11:07:15 +0000

    create starter question pages

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,23 @@
 const express = require('express');
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+// Date screener page routing
+router.post('/*/action/permission', function (req, res) {
+  // Set a variable to the answer
+  var answer = req.session.data['give-permission'];
+  // Set prototype version
+  var version = req.params[0];
+
+  // Check whether the answer is Yes, I give my permission
+  if (answer == "Yes, I give my permission"){
+    // Send user to next page
+    res.redirect(`/${version}/check-your-answers`)
+  }
+  else {
+    // Send user to enter a screen out page
+    res.redirect(`/${version}/no-permission-given`)
+  }
+
+})
 
 module.exports = router;
