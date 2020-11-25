92ebc747495fa53d7525ee21a56efed3d70739ff
AdamChrimes <iChrimes@live.co.uk>
2020-11-25 17:44:48 +0000

    add confirmation page for the permission question

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Date screener page routing
+// Do you give permission page routing
 router.post('/*/action/permission', function (req, res) {
   // Set a variable to the answer
   var answer = req.session.data['give-permission'];
@@ -15,10 +15,30 @@ router.post('/*/action/permission', function (req, res) {
     res.redirect(`/${version}/check-your-answers`)
   }
   else {
+    // Send user to a confirm permission page
+    res.redirect(`/${version}/do-you-give-permission-confirmation`)
+  }
+
+})
+
+// Confirm if you do not give permission page routing
+router.post('/*/action/permission-confirm', function (req, res) {
+  // Set a variable to the answer
+  var answer = req.session.data['give-permission-confirm'];
+  // Set prototype version
+  var version = req.params[0];
+
+  // Check whether the answer is I want to give my permission
+  if (answer == "I want to give my permission"){
+    // Send user to next page
+    res.redirect(`/${version}/do-you-give-permission`)
+  }
+  else {
     // Send user to enter a screen out page
     res.redirect(`/${version}/no-permission-given`)
   }
 
 })
+
 
 module.exports = router;
